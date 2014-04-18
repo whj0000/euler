7b3d077a7e7f0a1108b7cf4062ecb2e47013fbaf
whj0000 <robertwawnghaojia@gmail.com>
2014-04-18 13:05:18 +0800

    Project 1 solution
    Signed-off-by: whj0000 <robertwawnghaojia@gmail.com>

---
+++ Project Euler/src/Project1.java
@@ -0,0 +1,15 @@
+
+public class Project1 {
+
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=1;i<10;++i)
+		{
+			if(i%3==0||i%5==0){sum=sum+i;}
+		}
+		System.out.println("Total Sum Is "+sum);
+			
+	}
+
+}
+
